a121454d8da3ade1641957d3373a27ade69b505e
MichaelElisha1988 <mishaelifire@gmail.com>
2025-08-29 16:19:54 +0300

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule, HeaderComponent, FooterComponent, RouterOutlet],
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'elifire-website';
---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,72 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+export interface SiteRoutes {
+  title: string;
+  route: string;
+  isActive: boolean;
+}
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  private readonly router = inject(Router);
+
+  constructor() {
+    this.activateHeader();
+  }
+
+  listOfRoutes: SiteRoutes[] = [
+    {
+      title: 'home',
+      route: 'home',
+      isActive: false,
+    },
+    {
+      title: 'songs',
+      route: 'songs',
+      isActive: false,
+    },
+    // {
+    //   title: 'translations',
+    //   route: 'translations',
+    //   isActive: false,
+    // },
+    {
+      title: 'about',
+      route: 'about',
+      isActive: false,
+    },
+    {
+      title: 'support artist',
+      route: 'support-artist',
+      isActive: false,
+    },
+    {
+      title: 'contact me',
+      route: 'contact-me',
+      isActive: false,
+    },
+  ];
+
+  onNav(item: SiteRoutes) {
+    this.listOfRoutes.map((route) => {
+      route.isActive = false;
+    });
+    this.activateHeader(item.route);
+    this.router.navigate([item.route]);
+  }
+
+  activateHeader(route?: string) {
+    let activeHeader = route ? route : location.pathname.replace('/', '');
+    this.listOfRoutes.map((route) => {
+      route.route == activeHeader ? (route.isActive = true) : '';
+    });
+  }
+}
